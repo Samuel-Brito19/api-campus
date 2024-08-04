d23511d54e75d03148fb7048da0fb462abf4cb2a
Samuel-Brito19 <sambriton7@gmail.com>
2024-08-04 19:42:49 -0300

    Building our sever

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log("hello world")
+
+import fjwt, { JWT } from "@fastify/jwt";
+import Fastify, { FastifyReply, FastifyRequest } from "fastify";
+import cors from "@fastify/cors";
+
+declare module "fastify" {
+    interface FastifyRequest {
+        jwt: JWT
+    }
+    export interface FastifyInstance {
+        authenticate: any;
+    }
+}
+
+function buildServer() {
+    const fastify = Fastify({ logger: true });
+
+    fastify.register(cors, {
+        origin: "*"
+    })
+
+    fastify.register(fjwt, {secret: `${process.env.JWT_SECRET}`});
+
+    fastify.decorate('authenticate', async function (request: FastifyRequest, reply: FastifyReply) {
+        try {
+          await request.jwtVerify()
+        } catch (error) {
+          reply.send(error)
+        }
+      })
+
+    fastify.addHook("preHandler", (req, res, next) => {
+        req.jwt = fastify.jwt
+        return next()
+    })
+}
